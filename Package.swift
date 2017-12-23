451d5f91eb0a5661faa667632617b171621f3baf
iOSleep <mx.yolande@gmail.com>
2017-12-23 17:44:56 +0800

    修改依赖

--- Package.swift
+++ Package.swift
@@ -11,16 +11,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Carthage/Commandant", from: "0.12.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
-        .package(url: "https://github.com/xcodeswift/xcproj.git", .upToNextMajor(from: "0.2.0"))
+        .package(url: "https://github.com/xcodeswift/xcproj.git", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/iOSleep/CommandLine/", .branch("swift4-default")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "add_all_carthage",
-            dependencies: ["Rainbow", "Commandant", "xcproj"]),
-    ],
-    swiftLanguageVersions: [4]
+            dependencies: ["Rainbow", "CommandLine", "xcproj"]),
+    ]
 )
--- Sources/add_all_carthage/main.swift
+++ Sources/add_all_carthage/main.swift
@@ -1,22 +1,27 @@
 import Rainbow
-import Commandant
+import CommandLineKit
 import xcproj
 
 extension PBXShellScriptBuildPhase: CustomStringConvertible {
     public var description: String {
         var res = ""
-        res += "name: \(name ?? "")\n"
-        res += "shellPath \(shellPath)\n"
-        res += "shellScript \(shellScript ?? "")\n"
-        res += "runOnlyForDeploymentPostprocessing \(runOnlyForDeploymentPostprocessing)\n"
-        res += "shellScript:\n \(inputPaths)\n"
-        res += "shellScript:\n \(outputPaths)\n"
+        res += "name: \(name ?? "")\n".red
+        res += "shellPath: \(shellPath)\n".red
+        res += "shellScript: \(shellScript ?? "")\n".red
+        res += "runOnlyForDeploymentPostprocessing: \(runOnlyForDeploymentPostprocessing)\n".green
+        res += "inputPaths:\n\n \(inputPaths)\n\n".red
+        res += "outputPaths:\n\n \(outputPaths)\n\n".red
         return res
     }
 }
 
 let proj = try XcodeProj.init(path: "/Users/maxu/Desktop/TestDemo/TestDemo.xcodeproj")
 
-for a in proj.pbxproj.shellScriptBuildPhases{
+for a in proj.pbxproj.shellScriptBuildPhases {
+    guard let name = a.name, name == "Carthage" else {
+        continue
+    }
     print(a)
+    break
 }
+

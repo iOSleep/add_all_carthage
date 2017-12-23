51ed2bb4a7689a14d02c25aaebc8d128bfef3bb2
iOSleep <mx.yolande@gmail.com>
2017-12-23 17:24:59 +0800

    add CustomStringConvertible for PBXShellScriptBuildPhase

--- Sources/add_all_carthage/main.swift
+++ Sources/add_all_carthage/main.swift
@@ -2,3 +2,21 @@ import Rainbow
 import Commandant
 import xcproj
 
+extension PBXShellScriptBuildPhase: CustomStringConvertible {
+    public var description: String {
+        var res = ""
+        res += "name: \(name ?? "")\n"
+        res += "shellPath \(shellPath)\n"
+        res += "shellScript \(shellScript ?? "")\n"
+        res += "runOnlyForDeploymentPostprocessing \(runOnlyForDeploymentPostprocessing)\n"
+        res += "shellScript:\n \(inputPaths)\n"
+        res += "shellScript:\n \(outputPaths)\n"
+        return res
+    }
+}
+
+let proj = try XcodeProj.init(path: "/Users/maxu/Desktop/TestDemo/TestDemo.xcodeproj")
+
+for a in proj.pbxproj.shellScriptBuildPhases{
+    print(a)
+}
